869e7458d595bccfddde96daf202be01056fef6c
Luan Silva <luan@squarebits.com.br>
2018-01-29 17:31:01 -0200

    React calc

---
+++ index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import ReactCalculator from './src/ReactCalculator';
+
+import {
+    View,
+    Text,
+    AppRegistry
+} from 'react-native';
+
+AppRegistry.registerComponent('ReactCalculator', () => ReactCalculator);
